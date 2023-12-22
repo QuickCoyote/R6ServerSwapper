bddf95c154a4e4270023af6a01724b91271ca4f6
BrennanZDAS <jonathon_lau@hotmail.com>
2023-12-22 04:30:00 -0500

    Initial Commit :)
    This is the very simple version of the project

---
+++ R6ServerSwap/DataCenters.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+
+namespace R6ServerSwap
+{
+    public class DataCenters : ObservableCollection<string>
+    {
+        public DataCenters()
+        {
+            Add("Default (Ping Based)");
+            Add("Australia East");
+            Add("Brazil South");
+            Add("Central US");
+            Add("East Asia");
+            Add("East US");
+            Add("Japan East");
+            Add("North Europe");
+            Add("South Africa North");
+            Add("South Central US");
+            Add("South East Asia");
+            Add("UAE North");
+            Add("West Europe");
+            Add("West US");
+        }
+    }
+}
---
+++ R6ServerSwap/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace R6ServerSwap
+{
+    public partial class MainWindow : Window
+    {
+        string[] dataCenters = {
+            "default",
+            "australiaeast",
+            "brazilsouth",
+            "centralus",
+            "eastasia",
+            "eastus",
+            "japaneast",
+            "northeurope",
+            "southafricanorth",
+            "southcentralus",
+            "southeastasia",
+            "uaenorth",
+            "westeurope",
+            "westus"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string desiredDataCenter = dataCenters[comboBox1.SelectedIndex];
+
+            string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string basePath = userPath + "/Documents/My Games/Rainbow Six - Siege/";
+
+            string[] allAccounts = Directory.GetDirectories(basePath);
+            string[] gameSettingsLines = new string[0];
+
+            foreach (string account in allAccounts)
+            {
+                string filePath = account + "\\GameSettings.ini";
+
+                gameSettingsLines = File.ReadLines(filePath).ToArray();
+
+                gameSettingsLines = gameSettingsLines.Select(line => line.Contains("DataCenterHint=") ? "DataCenterHint=playfab/" + desiredDataCenter : line).ToArray();
+
+                File.Delete(account + "\\GameSettings.ini");
+                File.WriteAllLines(account + "\\GameSettings.ini", gameSettingsLines);
+
+                ConfirmationTextBlock.Text = "Changed Server To: " + comboBox1.SelectedValue;
+            }
+        }
+    }
+}
