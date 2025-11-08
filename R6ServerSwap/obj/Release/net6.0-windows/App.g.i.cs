89cd9a8dd1541c6c37e25cfab77be6d12932824f
Alexistential-Crisis <jonathon_lau@hotmail.com>
2025-11-08 13:36:37 -0600

    Build

--- R6ServerSwap/obj/Release/net6.0-windows/App.g.i.cs
+++ R6ServerSwap/obj/Release/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace R6ServerSwap {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace R6ServerSwap {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public static void Main() {
             R6ServerSwap.App app = new R6ServerSwap.App();
             app.InitializeComponent();
